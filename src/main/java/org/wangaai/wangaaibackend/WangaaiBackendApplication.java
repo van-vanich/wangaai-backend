6a558a1b8f1888a12edef04499506ab7ed4e37a5
van-vanich <panahno13@gmail.com>
2024-12-22 16:15:08 +0200

    init

---
+++ src/main/java/org/wangaai/wangaaibackend/WangaaiBackendApplication.java
@@ -0,0 +1,13 @@
+package org.wangaai.wangaaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangaaiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WangaaiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wangaai/wangaaibackend/WangaaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wangaai.wangaaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangaaiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
